396e4a3a937f12d1f83ef9cb461dd785d19539fa
Slixx01 <chrissiejohnson73@gmail.com>
2026-04-16 21:57:21 +0200

    fixed bittle necks and finished schedular as well

--- crawler/parser.py
+++ crawler/parser.py
@@ -12,9 +12,19 @@ from models.book import Book
 
 full_URL = 'https://books.toscrape.com/'
 
-async def scrape_book_details(client, book_url, limitation): 
-    async with limitation:
-        response = await client.get(book_url)
+async def scrape_book_details(client, book_url, limitation, raw_html_from_page): 
+    max_retires = 3
+    for attempt in range(max_retires):
+        try:
+            async with limitation:
+                response = await client.get(book_url)
+            break
+        except httpx.ReadTimeout:
+            if attempt == max_retires - 1:
+                print(f"Failed after {max_retires} attemps: {book_url}")
+                return None
+            print(f"Timeout on {book_url}, retyring... ({attempt +1}/{max_retires})")
+            await asyncio.sleep(2) 
 
     soup = BeautifulSoup(response.text, 'html.parser')
     title = soup.find('h1').text
@@ -56,7 +66,7 @@ async def scrape_book_details(client, book_url, limitation):
 
     rating_number = rating_map.get(rating_word, 0)
 
-    return {
+    book_data = {
         "name": title,
         "description": description_raw,
         "category": category_raw,
@@ -65,20 +75,28 @@ async def scrape_book_details(client, book_url, limitation):
         "availability": is_available,
         "num_reviews": num_reviews_final,
         "rating": rating_number,
-        "image_url": clean_image_url
+        "image_url": clean_image_url,
+        "source_url": book_url,
+        "raw_html": raw_html_from_page,
+        "status": "scraped",
+        "crawl_timestamp": datetime.now(UTC)
     }
 
- 
-async def generate_content_hash(book_data: dict) -> str: 
-        relevant_data = {
-            "name": book_data.get("name"),
-            "price_incl_tax": str(book_data.get("price_incl_tax")),
-            "availability": book_data.get("availability"),
-            "rating": book_data.get("rating")
-        }
+    relevant_data = {
+        "name": title,
+        "price_incl_tax": str(price_incl_tax_final),
+        "availability": is_available,
+        "rating": rating_number
+    }
+    data_string = json.dumps(relevant_data, sort_keys=True).encode('utf-8')
+    content_hash = hashlib.sha256(data_string).hexdigest()
+    
+    book_data['content_hash'] = content_hash
+    
+    book_model = Book(**book_data)
+    return book_model.model_dump(mode="json")
 
-        data_string = json.dumps(relevant_data, sort_keys=True).encode('utf-8')
-        return hashlib.sha256(data_string).hexdigest()
+
 
 
 async def main(): 
@@ -87,11 +105,10 @@ async def main():
     current_url = full_URL
     limitation = asyncio.Semaphore(5)
     
-    async with httpx.AsyncClient() as client: 
+    async with httpx.AsyncClient(timeout=30.0) as client: 
         while current_url:
             response = await client.get(current_url)
             soup = BeautifulSoup(response.text, "html.parser")
-            #Raw metadata  
             raw_html = response.text
             page_tasks = []
             book_urls = []
@@ -102,56 +119,43 @@ async def main():
                     href = anchor_tag.get('href')
                     book_url = urljoin(current_url, href)
                     book_urls.append(book_url)
-                    page_tasks.append(scrape_book_details(client, book_url, limitation))
+                    page_tasks.append(scrape_book_details(client, book_url, limitation, raw_html))
             print(f"Batch Scrape {len(page_tasks)} books...")
-            results = await asyncio.gather(*page_tasks)
+            results = [result for result in await asyncio.gather(*page_tasks) if result is not None]
             
             new_books = []
             content_check =  ["name", "availability", "rating", "price_incl_tax"]
            
 
-            for book_data, source_url in zip(results, book_urls):
-                 existing_book = await book_collection.find_one({"source_url":source_url})
-                 current_content_hash = await generate_content_hash(book_data)
+            for book_document in results:
+                source_url = book_document["source_url"]
+                current_content_hash = book_document["content_hash"]
+                
+                existing_book = await book_collection.find_one({"source_url": source_url})
 
-                 if book_data: 
-                    book_data['source_url'] = source_url
-                    book_data['raw_html'] = raw_html
-                    book_data['status'] = "scraped"
-                    book_data['crawl_timestamp'] = datetime.now(UTC)
-                    book_data['content_hash'] = current_content_hash
-                    
-                    book_model = Book(**book_data)
-                    book_document = book_model.model_dump(mode="json")
-                    
-
-                 if existing_book:
-
+                if existing_book:
                     if existing_book["content_hash"] != current_content_hash:
                         print(f"Changes found in {source_url}")
                         changed_values = {}
                         
                         for field in content_check:
-                            if book_data[field] != existing_book.get(field):
+                            if book_document[field] != existing_book.get(field):
                                 changed_values[field] = {
-                                    "old_value :" : existing_book.get(field),
-                                    "new_value :" : book_document[field]}
-                                
+                                    "old_value": existing_book.get(field),
+                                    "new_value": book_document[field]
+                                }
+                        
                         if changed_values: 
                             change_log = {
                                 "source_url": source_url,
-                                "timestamp" : datetime.now(UTC),
+                                "timestamp": datetime.now(UTC),
                                 "changes": changed_values
                             }
                             await change_log_detection.insert_one(change_log)
-                        await book_collection.replace_one({"source_url": source_url}, book_document)    
-                    else:
-                        print("No changes found")
-                 else:
-                     new_books.append(book_document)
-                
-                 
-                 
+                        
+                        await book_collection.replace_one({"source_url": source_url}, book_document)     
+                else:
+                    new_books.append(book_document)
 
             if new_books:
                 try:
@@ -160,7 +164,8 @@ async def main():
                     print(f"Successfully saved batch: Total saved{len(all_book_data)}")
                 except Exception as e:
                     print(f"Database error {e}")
-       
+
+            await asyncio.sleep(1)
             next_tag = soup.find('li',class_= 'next')
             if next_tag: 
                 next_ref = next_tag.find('a').get('href')
@@ -173,4 +178,3 @@ async def main():
         
 
 
-asyncio.run(main())
--- schedular/schedular.py
+++ schedular/schedular.py
@@ -1,9 +1,16 @@
+import asyncio
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from crawler.parser import main 
 
+async def run():
+    schedular = AsyncIOScheduler()
+    job_id = schedular.add_job(main, 'interval', seconds=10)
+    schedular.start()
 
-schedular = AsyncIOScheduler()
-job_id = schedular.add_job(main, 'interval', minutes=1)
-
-
-schedular.start()
+    try:
+        while True:
+            await asyncio.sleep(3600)
+    except (KeyboardInterrupt, SystemExit):
+        schedular.shutdown()
+asyncio.run(run())
